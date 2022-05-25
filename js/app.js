e58b8ff446c69b1c9d69c50b51fc7892bce393aa
Ekko <48669764+TiMEkko0813@users.noreply.github.com>
2022-05-25 15:27:53 +0800

    提交

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/webAR.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/1110634024.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
